eac16c6399365b48d6cd27da2c8d95e128d2343e
lokeshkumar3103ar <130975498+lokeshkumar3103ar@users.noreply.github.com>
2025-03-28 15:50:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()  {
+    var member = new Object();
+    member.name = "John";
+    member.age = 30;
+    member.skills = ["JavaScript", "HTML", "CSS"];
+    return member;
+}
+module.exports = skillsMember;  
+                        
